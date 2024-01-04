d40b043025c42012a1ee2f65f340fc24a4abd359
himanshi <himanshigaire662@gmail.com>
2024-01-04 17:36:04 +0545

    about us

--- js/aboutus.js
+++ js/aboutus.js
@@ -23,3 +23,4 @@ ScrollReveal().reveal(".footer", {
 
 
 
+
---
+++ js/counter.js
@@ -0,0 +1,19 @@
+const counterUp = window.counterUp.default
+
+const callback = entries => {
+	entries.forEach( entry => {
+		const el = entry.target
+		if ( entry.isIntersecting && ! el.classList.contains( 'is-visible' ) ) {
+			counterUp( el, {
+				duration: 1400,
+				delay: 16,
+			} )
+			el.classList.add( 'is-visible' )
+		}
+	} )
+}
+
+const IO = new IntersectionObserver( callback, { threshold: 1 } )
+
+const el = document.querySelector( '.counter' )
+IO.observe( el )
