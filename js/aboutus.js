64cd70e0f1c8f8313afb24ad0ed17f66314aa79f
himanshi <himanshigaire662@gmail.com>
2024-01-04 18:37:02 +0545

    about us finished

--- js/aboutus.js
+++ js/aboutus.js
@@ -21,6 +21,76 @@ ScrollReveal().reveal(".footer", {
     easing: "ease-in-out",
 });
 
+ScrollReveal().reveal(".about-us-heading", {
+    delay: 0,
+    duration: 1400,
+    origin: "bottom",
+    distance: "200px",
+    easing: "ease-in-out",
+});
+
+ScrollReveal().reveal(".about-us-illustration", {
+    delay: 200,
+    duration: 1400,
+    origin: "bottom",
+    distance: "200px",
+    easing: "ease-in-out",
+});
+
+ScrollReveal().reveal(".about-us-content p", {
+    delay: 400,
+    duration: 1400,
+    origin: "bottom",
+    distance: "200px",
+    easing: "ease-in-out",
+});
+
+ScrollReveal().reveal(".infographic-container", {
+    delay: 0,
+    duration: 1000,
+    easing: "ease-in-out",
+});
+
+ScrollReveal().reveal(".values-heading h1", {
+    delay: 0,
+    duration: 1400,
+    origin: "bottom",
+    distance: "200px",
+    easing: "ease-in-out",
+});
+
+ScrollReveal().reveal(".values-heading p", {
+    delay: 200,
+    duration: 1400,
+    origin: "bottom",
+    distance: "200px",
+    easing: "ease-in-out",
+});
+
+ScrollReveal().reveal(".column", {
+    delay: 300,
+    duration: 1400,
+    origin: "bottom",
+    distance: "200px",
+    easing: "ease-in-out",
+    interval: 200
+});
+
+ScrollReveal().reveal(".quote-container", {
+    delay: 300,
+    duration: 1000,
+    easing: "ease-in-out",
+});
+
+ScrollReveal().reveal(".timeline-container", {
+    delay: 200,
+    duration: 1400,
+    origin: "bottom",
+    distance: "200px",
+    easing: "ease-in-out",
+});
+
+
 
 
 
