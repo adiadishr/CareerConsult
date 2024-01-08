665658b54e64f6450f3ec99d6d5d3ca0ab661b60
himanshi <himanshigaire662@gmail.com>
2024-01-08 14:56:30 +0545

    contact us page done

--- js/contact.js
+++ js/contact.js
@@ -21,6 +21,38 @@ ScrollReveal().reveal(".footer", {
     easing: "ease-in-out",
 });
 
+ScrollReveal().reveal(".filler h1", {
+    delay: 200,
+    duration: 1400,
+    origin: "bottom",
+    distance: "200px",
+    easing: "ease-in-out",
+});
+
+ScrollReveal().reveal(".filler h2", {
+    delay: 400,
+    duration: 1400,
+    origin: "bottom",
+    distance: "200px",
+    easing: "ease-in-out",
+});
+
+ScrollReveal().reveal(".filler p", {
+    delay: 600,
+    duration: 1400,
+    origin: "bottom",
+    distance: "200px",
+    easing: "ease-in-out",
+});
+
+ScrollReveal().reveal(".filler .imageholder", {
+    delay: 600,
+    duration: 1400,
+    origin: "bottom",
+    distance: "200px",
+    easing: "ease-in-out",
+});
+
 ScrollReveal().reveal(".contact-heading", {
     delay: 200,
     duration: 1400,
